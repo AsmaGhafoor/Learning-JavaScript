9d863be91f4123561ab95844603628994b7f6d53
coding <asmaghafoor200@gmail.com>
2022-08-09 18:26:23 +0500

    first commit

---
+++ index21.js
@@ -0,0 +1,14 @@
+console.log('INDEX 21');
+
+/* 
+You have to create a div and inject it into the page which 
+contains the heading.
+whenever someone clicks on ht div,it should be converted into 
+an editable item.
+Wheneever usr clicks away(blur). save the note into 
+th local Storage.
+*/
+
+
+
+
